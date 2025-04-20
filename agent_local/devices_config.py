a60a7b25d64ec1ae86155c608aca507a5cd3a6a9
minhduc <minhducpvm@gmail.com>
2025-04-20 22:43:20 +0700

    update

--- agent_local/devices_config.py
+++ agent_local/devices_config.py
@@ -1,8 +1,8 @@
 USING_DEVICES = [
     {
         "host": "lab57.vnpro.org",       # hoặc "113.161.103.57"
-        "username": "minhduc",
-        "password": "gaWBQNrK",
+        "username": "admin",
+        "password": "cisco",
         # Nếu port SSH khác 22 thì thêm dòng sau:
         # "port": 2222,
     }
